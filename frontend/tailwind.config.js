e865327d3df71e90930d109001a79f28e6c29a40
Dil Nawaz Khan <157898931+Dilnawazkhan2@users.noreply.github.com>
2025-02-16 20:57:36 +0500

    project completed

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+
+export default {
+  content: ["./index.html", "./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      "light",
+      "dark",
+      
+    ],
+  },
+};
